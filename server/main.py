543c059c92a14c5d05f9ec8722a9ea649caf2cc8
goodmice <av.sirotkin@digitaldrilling.ru>
2023-06-24 19:15:25 +0500

    Добавлена проверка на авторизацию для POST, PUT и DELETE методов

--- server/main.py
+++ server/main.py
@@ -1,5 +1,3 @@
-from typing import Annotated
-
 from fastapi import Depends, FastAPI, HTTPException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
@@ -38,36 +36,36 @@ def generate_token(input_data : OAuth2PasswordRequestForm = Depends(), db: Sessi
     return { 'access_token': token, 'token_type': 'bearer' }
 
 
+@app.get('/languages/', response_model=list[schemas.Language], tags=['Languages'])
+def get_languages(db: Session = Depends(get_db)):
+    return crud.get_languages(db)
+
+
 @app.post('/languages/', response_model=schemas.Language, tags=['Languages'])
-def create_language(language: schemas.LanguageCreate, db: Session = Depends(get_db)):
+def create_language(language: schemas.LanguageCreate, db: Session = Depends(get_db), token: str = Depends(oauth2)):
     db_languages = crud.get_languages(db)
     if language.title in [lang.title for lang in db_languages]:
         raise HTTPException(status_code=400, detail='Language already exists')
     return crud.create_language(db, language)
 
 
-@app.get('/languages/', response_model=list[schemas.Language], tags=['Languages'])
-def get_languages(db: Session = Depends(get_db)):
-    return crud.get_languages(db)
-
-
 @app.delete('/languages/{language_id}', response_model=bool, tags=['Languages'])
-def delete_language(language_id: int, db: Session = Depends(get_db)):
+def delete_language(language_id: int, db: Session = Depends(get_db), token: str = Depends(oauth2)):
     return crud.delete_language(db, language_id)
 
 
 @app.put('/languages/{language_id}', response_model=bool, tags=['Languages'])
-def update_language(language_id: int, language: schemas.LanguageCreate, db: Session = Depends(get_db)):
+def update_language(language_id: int, language: schemas.LanguageCreate, db: Session = Depends(get_db), token: str = Depends(oauth2)):
     return crud.update_language(db, language_id, language)
 
 
 @app.get('/users/{user_id}', response_model=schemas.User, tags=['Users'])
-def get_user(user_id: int, db: Session = Depends(get_db)):
+def get_user(user_id: int, db: Session = Depends(get_db), token: str = Depends(oauth2)):
     return crud.get_user(db, user_id)
 
 
 @app.post('/users/', response_model=schemas.User, tags=['Users'])
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db), token: str = Depends(oauth2)):
     db_user = crud.get_user_by_login(db, user.login)
     if db_user:
         raise HTTPException(status_code=400, detail='Login already registered')
@@ -80,7 +78,7 @@ def get_topics(db: Session = Depends(get_db)):
 
 
 @app.post('/topics/', response_model=schemas.Topic, tags=['Topics'])
-def create_topic(topic: schemas.TopicCreate, db: Session = Depends(get_db)):
+def create_topic(topic: schemas.TopicCreate, db: Session = Depends(get_db), token: str = Depends(oauth2)):
     db_topics = crud.get_topics()
     if topic.title in [t.title for t in db_topics]:
         raise HTTPException(status_code=400, detail='Topic already exists')
@@ -88,12 +86,12 @@ def create_topic(topic: schemas.TopicCreate, db: Session = Depends(get_db)):
 
 
 @app.put('/topics/{topic_id}', response_model=bool, tags=['Topics'])
-def update_topic(topic_id: int, topic: schemas.TopicCreate, db: Session = Depends(get_db)):
+def update_topic(topic_id: int, topic: schemas.TopicCreate, db: Session = Depends(get_db), token: str = Depends(oauth2)):
     return crud.update_topic(db, topic_id, topic)
 
 
 @app.delete('/topics/{topic_id}', response_model=bool, tags=['Topics'])
-def delete_topic(topic_id: int, db: Session = Depends(get_db)):
+def delete_topic(topic_id: int, db: Session = Depends(get_db), token: str = Depends(oauth2)):
     return crud.delete_topic(db, topic_id)
 
 
@@ -103,15 +101,15 @@ def get_word(word_id: int, db: Session = Depends(get_db)):
 
 
 @app.post('/words/', response_model=schemas.Word, tags=['Words'])
-def create_word(word: schemas.WordCreate, db: Session = Depends(get_db)):
+def create_word(word: schemas.WordCreate, db: Session = Depends(get_db), token: str = Depends(oauth2)):
     return crud.create_word(db, word)
 
 
 @app.put('/words/{word_id}', response_model=bool, tags=['Words'])
-def update_word(word_id: int, word: schemas.WordCreate, db: Session = Depends(get_db)):
+def update_word(word_id: int, word: schemas.WordCreate, db: Session = Depends(get_db), token: str = Depends(oauth2)):
     return crud.update_word(db, word_id, word)
 
 
 @app.delete('/words/{word_id}', response_model=bool, tags=['Words'])
-def update_word(word_id: int, db: Session = Depends(get_db)):
+def update_word(word_id: int, db: Session = Depends(get_db), token: str = Depends(oauth2)):
     return crud.delete_word(db, word_id)
