db0ddf535d2aa1547e807bcd02ddebf9013dfca5
goodmice <av.sirotkin@digitaldrilling.ru>
2023-06-26 06:57:52 +0500

    * Добавлен русский язык * Исправлена высота на мобильных устройствах * Записан токен в OpenAPI

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,23 @@
 import ReactDOM from 'react-dom'
+import { ConfigProvider } from 'antd-mobile'
+
+import { getUser } from './utils/functions'
+import { OpenAPI } from './api'
+import ruRU from './ruRU'
 
 import App from './App'
 
 import './styles/index.less'
 
+const vh = window.innerHeight * 0.01
+document.documentElement.style.setProperty('--vh', `${vh}px`)
+
+OpenAPI.TOKEN = async () => getUser()?.access_token || ''
+
 const el = document.getElementById('app')
 
 ReactDOM.render((
-    <App />
+    <ConfigProvider locale={ruRU}>
+        <App />
+    </ConfigProvider>
 ), el)
