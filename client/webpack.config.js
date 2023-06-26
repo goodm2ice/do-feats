f69724e12e5fcbe2c5a89bb212bc26707b0841e5
goodmice <av.sirotkin@digitaldrilling.ru>
2023-06-26 06:51:31 +0500

    Улучшены конфигурации фронта, добавлена работа прокси

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,24 +1,51 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
+const proxy = require('./package.json').proxy
+
 module.exports = {
+    entry: './src/index.tsx',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
+        filename: '[name].[contenthash:8].js',
+        publicPath: '/',
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: 'static/index.html',
-        }),
+        new HtmlWebpackPlugin({ template: './static/index.html' }),
     ],
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                },
+            },
+        },
+        removeAvailableModules: true,
+        removeEmptyChunks: true,
+        mergeDuplicateChunks: true,
+        providedExports: true,
+        runtimeChunk: true,
+    },
     devServer: {
+        historyApiFallback: true,
         port: 5000,
         static: {
             directory: path.resolve(__dirname, 'static'),
         },
         proxy: {
-            '/api': 'http://localhost:3000',
-        }
+            context: ['/api'],
+            target: proxy,
+        },
+        client: {
+            overlay: {
+                errors: true,
+                warnings: false,
+            },
+            progress: true,
+        },
     },
     resolve: {
         extensions: ['.tsx', '.ts', '.js'],
